3e9051cc4807a3ac1f63f7fdd5aa10c17938dd86
yukvx <Betrval@gmail.com>
2024-11-14 17:42:11 -0800

    Update and rename pcallMeIrlPls.lua to WillYoUpcallMe.lua

--- gamesneeze/WillYoUpcallMe.lua
+++ gamesneeze/WillYoUpcallMe.lua
@@ -116,7 +116,7 @@ local theme = {
     textborder = Color3.fromRGB(0, 0, 0),
     cursoroutline = Color3.fromRGB(10, 10, 10),
     font = 1,
-    textsize = 13
+    textsize = 11
 }
 -- // utility Functions
 do
