90133d51a37056a7773280a7d6fab91e9b7c31c5
yukvx <Betrval@gmail.com>
2024-11-14 17:45:53 -0800

    Update and rename diddy.lua to ripJaydes.lua

--- gamesneeze/ripJaydes.lua
+++ gamesneeze/ripJaydes.lua
@@ -116,7 +116,7 @@ local theme = {
     textborder = Color3.fromRGB(0, 0, 0),
     cursoroutline = Color3.fromRGB(10, 10, 10),
     font = 1,
-    textsize = 12.6
+    textsize = 12.3
 }
 -- // utility Functions
 do
